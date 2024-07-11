4079ad43ae2dc7e4d75fdff28bae8dfa0d830c29
Bagus Aji Santoso <lobothijau@gmail.com>
2024-07-11 13:21:23 +0700

    Add key to loop

--- app/attendance_page.tsx
+++ app/attendance_page.tsx
@@ -72,7 +72,7 @@ const AttendancePage = () => {
                     className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
                   >
                     {employees.map((employee) => (
-                      <option value={employee.id}>{employee.name}</option>
+                      <option key={employee.id} value={employee.id}>{employee.name}</option>
                     ))}
                   </select>
                 </div>
--- app/report_page.tsx
+++ app/report_page.tsx
@@ -49,7 +49,7 @@ const ReportPage = () => {
         </thead>
         <tbody>
           {employees.map((employee) => (
-            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+            <tr key={employee.id} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
               <th
                 scope="row"
                 className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
